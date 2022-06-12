3a6e35bc32d558f51418f2f6f958ad95fd9845b0
Dmitry Fedorinov <fedorinov.dmitry@gmail.com>
2022-06-12 12:07:42 +0300

    Initial Commit

---
+++ SwiftUIEducationIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIEducationIOS
+//
+//  Created by Дмитрий Федоринов on 12.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIEducationIOS/SwiftUIEducationIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEducationIOSApp.swift
+//  SwiftUIEducationIOS
+//
+//  Created by Дмитрий Федоринов on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEducationIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
